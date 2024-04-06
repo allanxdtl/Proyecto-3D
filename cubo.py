8ccf2541aac56eac82273e52a64bbd0e78b426f8
BRYAN ALLAN VALDEZ MUÑOZ <L19340534@nogales.tecnm.mx>
2024-04-05 20:51:21 -0700

    Cubos

---
+++ cubo.py
@@ -0,0 +1,9 @@
+from OpenGL.GL import *
+from OpenGL.GLUT import *
+from OpenGL.GLU import *
+
+class Cube:
+    
+    def drawCubo(self, color):
+        glColor3f(color[0]/255, color[1]/255, color[2]/255)
+        glutSolidCube(1.0)
