8d39eb374df197ad84d1ec1385601d664c5f1321
JoseGarcia0609 <21340485@itnogales.edu.mx>
2024-04-22 12:59:14 -0700

    Brazo izquierdo

--- brazos.py
+++ brazos.py
@@ -12,4 +12,15 @@ class Brazos:
         circulos = Sphere()
         
         #Brazo izquierdo
-        
+        glPushMatrix()
+        glTranslate(-4, -1.2, -0.050)
+        glRotate(90, 0, 1, 0)
+        glRotate(-15, 1, 0, 0)
+        cilindritos.drawCilindro([255,255,255], 0.6, 1.5, 100, 100)
+        glTranslate(0, -0.2, -1)
+        glRotate(-8, 1, 0, 0)
+        cilindritos.drawCilindro([255,255,255], 0.6, 1.5, 100, 100)
+        glTranslate(0, 0, -0.5)
+        cilindritos.drawCilindro([255,255,255], 0.6, 0.5, 100, 100)
+        circulos.drawSphere([255,255,255], 0.6, 100, 100)
+        glPopMatrix()
